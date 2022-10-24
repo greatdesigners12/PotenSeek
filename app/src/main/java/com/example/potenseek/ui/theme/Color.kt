3dc9f6e8da0c057998cb9e16909bd1af2eb9d80b
RDJinTropicThunder <nevolve27@gmail.com>
2022-10-24 11:28:17 +0700

    Add Color and Font

--- app/src/main/java/com/example/potenseek/ui/theme/Color.kt
+++ app/src/main/java/com/example/potenseek/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//Pie Chart Color
+val orderOne = Color(0xFFFF4E47)
+val orderTwo = Color(0xFFFFAA47)
+val orderThree = Color(0xFF992F2B)
+val orderFour = Color(0xFFFDDA5D)
+val orderFive = Color(0xFFE9FFAC)
--- app/src/main/java/com/example/potenseek/ui/theme/Type.kt
+++ app/src/main/java/com/example/potenseek/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.example.potenseek.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.potenseek.R
+
+val OpenSans = FontFamily(
+    Font(R.font.opensans_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
